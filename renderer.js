058b3ae725b8adea2d700ecdcfe345c842667762
yuuFen <603236742@qq.com>
2020-02-21 21:09:21 +0800

    文件操作、拖放

--- renderer.js
+++ renderer.js
@@ -4,3 +4,21 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const fs = require('fs')
+
+const dragWrapper = document.getElementById('file-drag')
+dragWrapper.addEventListener('drop', (e) => {
+  e.preventDefault() // 防止页面迁移（比如打开了拖入的html）
+  const files = e.dataTransfer.files
+  if (files && files.length > 0) {
+    const path = files[0].path
+    console.log(path)
+    const content = fs.readFileSync(path)
+    console.log(content.toString())
+  }
+})
+
+dragWrapper.addEventListener('dragover', (e) => {
+  e.preventDefault()
+})
